dc05aab78fe7c021b535dce3e3d85dffaa6869be
Patrick Walters <patrickwaltersc21@yahoo.com>
2015-05-07 22:37:27 -0500

    fix(modal): Fix jspm config.js

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ System.config({
     "babel": "npm:babel-core@5.2.16",
     "babel-runtime": "npm:babel-runtime@5.1.11",
     "core-js": "npm:core-js@0.8.4",
+    "jquery": "github:components/jquery@2.1.3",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
     "github:jspm/nodelibs-process@0.1.1": {
@@ -25,3 +26,4 @@ System.config({
     }
   }
 });
+
