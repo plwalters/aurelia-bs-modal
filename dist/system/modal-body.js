1709b8340d55742070f31e9e7ad49853b97a7f91
Mike <lipsitzm@gmail.com>
2015-05-28 19:13:08 -0400

    adding the ability to bind the model object in addition to the view-model

--- dist/system/modal-body.js
+++ dist/system/modal-body.js
@@ -1,4 +1,6 @@
 System.register(['aurelia-framework'], function (_export) {
+  'use strict';
+
   var bindable, ModalBody;
 
   var _createDecoratedClass = (function () { function defineProperties(target, descriptors, initializers) { for (var i = 0; i < descriptors.length; i++) { var descriptor = descriptors[i]; var decorators = descriptor.decorators; var key = descriptor.key; delete descriptor.key; delete descriptor.decorators; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor || descriptor.initializer) descriptor.writable = true; if (decorators) { for (var f = 0; f < decorators.length; f++) { var decorator = decorators[f]; if (typeof decorator === 'function') { descriptor = decorator(target, key, descriptor) || descriptor; } else { throw new TypeError('The decorator for method ' + descriptor.key + ' is of the invalid type ' + typeof decorator); } } if (descriptor.initializer !== undefined) { initializers[key] = descriptor; continue; } } Object.defineProperty(target, key, descriptor); } } return function (Constructor, protoProps, staticProps, protoInitializers, staticInitializers) { if (protoProps) defineProperties(Constructor.prototype, protoProps, protoInitializers); if (staticProps) defineProperties(Constructor, staticProps, staticInitializers); return Constructor; }; })();
@@ -12,19 +14,24 @@ System.register(['aurelia-framework'], function (_export) {
       bindable = _aureliaFramework.bindable;
     }],
     execute: function () {
-      'use strict';
-
       ModalBody = (function () {
         var _instanceInitializers = {};
 
         function ModalBody() {
           _classCallCheck(this, ModalBody);
 
-          _defineDecoratedPropertyDescriptor(this, 'content', _instanceInitializers);
+          _defineDecoratedPropertyDescriptor(this, 'content_view', _instanceInitializers);
+
+          _defineDecoratedPropertyDescriptor(this, 'content_model', _instanceInitializers);
         }
 
         _createDecoratedClass(ModalBody, [{
-          key: 'content',
+          key: 'content_view',
+          decorators: [bindable],
+          initializer: null,
+          enumerable: true
+        }, {
+          key: 'content_model',
           decorators: [bindable],
           initializer: null,
           enumerable: true
