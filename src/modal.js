663c0977d15fb23fad4896128991b8ca0b1e160d
Ats Uiboupin <ats.uiboupin@iglu.ee>
2015-10-15 01:06:21 +0300

    #16 - implemented option to pass escape handler function to the modal using `<modal escape-callback.call="customEscapeCallback()">`

--- src/modal.js
+++ src/modal.js
@@ -5,23 +5,55 @@ import $ from 'bootstrap';
 @inject(Element)
 export class Modal {
   @bindable showing = false;
+  @bindable escapeCallback /*: () => void*/;
+  keydownHandler = null;
+
   constructor(element) {
     this.element = element;
   }
+
   attached(){
     $(this.modal).modal({show: false})
-    .on('show.bs.modal', () => {	
+    .on('show.bs.modal', () => {
       this.showing = true;
     })
-    .on('hide.bs.modal', () => {	
+    .on('hide.bs.modal', () => {
       this.showing = false;
     });
+    if(this.showing) {
+      this.addEscHandler();
+    }
   }
+
+  detached() {
+    this.removeEscHandler();
+  }
+
   showingChanged(newValue){
     if (newValue) {
+      this.addEscHandler();
       $(this.modal).modal('show')
     } else {
       $(this.modal).modal('hide')
+      this.removeEscHandler();
+    }
+  }
+
+  addEscHandler() {
+    if(this.escapeCallback && !this.keydownHandler) {
+      this.keydownHandler = (e /*: JQueryEventObject*/) => {
+        if(e.which == 27) { // escape was pressed
+          this.escapeCallback();
+        }
+      };
+      $(this.modal).on("keydown.dismiss.bs.modal", this.keydownHandler);
+    }
+  }
+
+  removeEscHandler() {
+    if(this.keydownHandler) {
+      $(this.modal).off("keydown.dismiss.bs.modal", this.escapeCallback);
+      this.keydownHandler = null;
     }
   }
 }
